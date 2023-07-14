e4e635b15ee9c82bec059f5dc7afb190346be734
kabir-1618 <96172488+kabir-1618@users.noreply.github.com>
2023-07-14 11:27:56 +0530

    Update Middle of linked list.C

--- Middle of linked list.C
+++ Middle of linked list.C
@@ -1,11 +1,1 @@
-struct ListNode* middleNode(struct ListNode* head) {
-    struct ListNode* slow = head;
-    struct ListNode* fast = head;
-    
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    
-    return slow;
-}
+
